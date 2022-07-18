00463142d59b9c219ff9f6e7660723b827eb104c
yandrakarine <yandrakarine.dev@outlook.com>
2022-07-18 16:13:40 -0300

    add fetch method to get pokemon info

---
+++ app.js
@@ -0,0 +1,33 @@
+const fetchPokemon = () => {
+  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+  const pokemonPromises = []
+
+  for (let i = 1; i <= 150; i++) {
+    pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+
+  }
+
+  Promise.all(pokemonPromises)
+    .then(pokemons => {
+      // console.log(pokemons)
+
+      const listPokemons = pokemons.reduce((accumulator, pokemon) => {
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+        accumulator += `
+
+        <li class="card">
+        <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+        <p class="card-subtitle">${types.join(' | ')}</p>
+        </li>
+        `
+        return accumulator
+      }, '')
+
+      console.log(listPokemons)
+
+    })
+}
+
+fetchPokemon()
