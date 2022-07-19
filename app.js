7e008b24b4618becb1a83bd9f9b8b37a7e6b6edd
yandrakarine <yandrakarine.dev@outlook.com>
2022-07-19 19:26:32 -0300

    fix layout card

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const fetchPokemon = () => {
         const types = pokemon.types.map(typeInfo => typeInfo.type.name)
         accumulator += `
 
-        <li class="card">
-        <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+        <li class="card ${types[0]}">
+        <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
         <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
         <p class="card-subtitle">${types.join(' | ')}</p>
         </li>
