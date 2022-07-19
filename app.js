4de77d59c3d1c2758a44cc9ce65e73619bf0cd9a
yandrakarine <yandrakarine.dev@outlook.com>
2022-07-19 19:17:27 -0300

    add pokemons images in html

--- app.js
+++ app.js
@@ -10,14 +10,13 @@ const fetchPokemon = () => {
 
   Promise.all(pokemonPromises)
     .then(pokemons => {
-      // console.log(pokemons)
 
       const listPokemons = pokemons.reduce((accumulator, pokemon) => {
         const types = pokemon.types.map(typeInfo => typeInfo.type.name)
         accumulator += `
 
         <li class="card">
-        <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+        <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
         <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
         <p class="card-subtitle">${types.join(' | ')}</p>
         </li>
@@ -26,6 +25,10 @@ const fetchPokemon = () => {
       }, '')
 
       console.log(listPokemons)
+
+      const ul = document.querySelector('[data-js="pokedex"]')
+
+      ul.innerHTML = listPokemons
 
     })
 }
